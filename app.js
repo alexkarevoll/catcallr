93394de0cc3a5e16a41d861e7752c367de66a83c
Alex Karevoll <alex.karevoll@gmail.com>
2016-08-28 16:02:26 -0700

    initial pseudocode

---
+++ app.js
@@ -0,0 +1,13 @@
+// Define global variables
+
+// Create a moves object with four moves and their various strengths
+
+// Create a cat object with weaknesses to certain kinds of moves
+
+// Logic for player choosing a move
+
+// Logic for comparing chosen move to cat's weakness
+
+// Logic for moving cat based on the comparison of weakness to user move
+
+// Logic for declaring a winner if cat reaches player goal line
