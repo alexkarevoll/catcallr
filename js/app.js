2c7e060ead502ad241629e4825d84f4b475270c3
Alex Karevoll <alex.karevoll@gmail.com>
2016-08-29 12:39:36 -0700

    added cat to play field, created moves object

--- js/app.js
+++ js/app.js
@@ -1,7 +1,53 @@
-
 // Define global variables
 
-// Create a moves object with four moves and their various strengths
+// Create a moves object with three categories of three moves and their various strengths
+
+var moves = {};
+
+moves.reason = {
+  reason1 : {
+    text: "reason1text",
+    str: 2,
+  },
+  reason2 : {
+    text: "reason2text",
+    str: 4,
+  },
+  reason3 : {
+    text: "reason3text",
+    str: 6,
+  },
+}
+
+moves.amuse = {
+  reason1 : {
+    text: "amuse1text",
+    str: 2,
+  },
+  reason2 : {
+    text: "amuse2text",
+    str: 4,
+  },
+  reason3 : {
+    text: "amuse3text",
+    str: 6,
+  },
+}
+
+moves.treat = {
+  reason1 : {
+    text: "treat1text",
+    str: 2,
+  },
+  reason2 : {
+    text: "treat2text",
+    str: 4,
+  },
+  reason3 : {
+    text: "treat3text",
+    str: 6,
+  },
+}
 
 // Create a cat object with weaknesses to certain kinds of moves
 
