3ac50bb60cd5e920a3c86449b3d1913c1d9d7e2b
Alex Karevoll <alex.karevoll@gmail.com>
2016-08-29 11:27:09 -0700

    basic layout

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
+
 // Define global variables
 
 // Create a moves object with four moves and their various strengths
 
 // Create a cat object with weaknesses to certain kinds of moves
 
+// MOVE SELECTION SPACE //
 // Logic for player choosing a move
 
 // Logic for comparing chosen move to cat's weakness
 
-// Logic for moving cat based on the comparison of weakness to user move
+// Logic for moving cat based on the comparison of weakness to user move, and printing status message to players
 
 // Logic for declaring a winner if cat reaches player goal line
+
+// Restart game button
