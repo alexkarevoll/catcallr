ce5edff4070599c92bf464345d2c59723f58b4bd
Alex Karevoll <alex.karevoll@gmail.com>
2016-08-29 15:32:43 -0700

    added move set randomizer

--- js/app.js
+++ js/app.js
@@ -1,58 +1,99 @@
 // Define global variables
 
+var turn = true;
+var currentMoveSet = [];
+var playerMove = {};
+
+$choices = $('.choice');
+
 // Create a moves object with three categories of three moves and their various strengths
 
-var moves = {};
-
-moves.reason = {
-  reason1 : {
+var moves = [
+  {
     text: "reason1text",
     str: 2,
+    type: 'reason',
   },
-  reason2 : {
+  {
     text: "reason2text",
     str: 4,
+    type: 'reason',
   },
-  reason3 : {
+  {
     text: "reason3text",
     str: 6,
+    type: 'reason',
   },
-}
-
-moves.amuse = {
-  reason1 : {
+  {
     text: "amuse1text",
     str: 2,
+    type: 'amuse',
   },
-  reason2 : {
+  {
     text: "amuse2text",
     str: 4,
+    type: 'amuse',
   },
-  reason3 : {
+  {
     text: "amuse3text",
     str: 6,
+    type: 'amuse',
   },
-}
-
-moves.treat = {
-  reason1 : {
+  {
     text: "treat1text",
     str: 2,
+    type: 'treat',
   },
-  reason2 : {
+  {
     text: "treat2text",
     str: 4,
+    type: 'treat',
   },
-  reason3 : {
+  {
     text: "treat3text",
     str: 6,
-  },
+    type: 'treat',
+  }
+];
+
+// Create a cat object with weaknesses to certain kinds of moves.
+// In the future it would be awesome to have random cat stats or multiple cats
+
+var cat = {};
+
+cat.bias ={
+  reason : .5,
+  amuse : 1,
+  treat : 1.5,
 }
 
-// Create a cat object with weaknesses to certain kinds of moves
+// Function that picks a random item out of a given array no matter how long
+function pickRandom(arr) {
+  return arr[Math.floor(Math.random()*arr.length)]
+};
 
 // MOVE SELECTION SPACE //
+// Function to populate board with four new random moves
+function newMoves(){
+
+  for (var i = 0; i<$choices.length; i++){
+    currentMoveSet.push(pickRandom(moves))
+  }
+  $('#choice-one').text(currentMoveSet[0].text);
+  $('#choice-two').text(currentMoveSet[1].text);
+  $('#choice-three').text(currentMoveSet[2].text);
+  $('#choice-four').text(currentMoveSet[3].text);
+}
+
+
 // Logic for player choosing a move
+function setMove(){
+  if
+  $('#choice-one').text(currentMoveSet[0].text);
+  $('#choice-two').text(currentMoveSet[1].text);
+  $('#choice-three').text(currentMoveSet[2].text);
+  $('#choice-four').text(currentMoveSet[3].text);
+}
 
 // Logic for comparing chosen move to cat's weakness
 
